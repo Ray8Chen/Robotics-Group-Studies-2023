5c55a3a4bd31edf4b57e55d138a620b7e930b012
Ray8Chen <125579054+Ray8Chen@users.noreply.github.com>
2023-02-28 16:51:06 +0000

    Update Control_API_V0.py

--- Control System/Control_API_V0.py
+++ Control System/Control_API_V0.py
@@ -7,21 +7,20 @@ This file should:
 NOTE: WHEN WRITING THIS PROGRAM PLEASE RE-UPLOAD TO THE GITHUB AS Control_API_V1.py and increment as needed
 """
 
-import socket
+import socket, pickle
 from naoqi import ALProxy
-
 
 # main loop - should'nt need to be changed much
 def main(PORT_NUMBER = 10000,NAO_IP = "192.168.1.3"):
   
-  TCP_connection = TCP_connect(PORT_NUMBER)
+  TCP_connection, TCP_socket = TCP_connect(PORT_NUMBER)
   NAO_connection = NAO_connect(NAO_IP)
 
   while True:
-    action = get_action(TCP_connection)
+    action = get_action(TCP_connection, TCP_socket)
     move_NAO(action)
     NAO_info = get_NAO_info(NAO_connection)
-    send(NAO_info)
+    send(NAO_info, TCP_socket)
     
     
 # -------------------------------------------------------- NAO stuff:  
@@ -43,18 +42,31 @@ def get_NAO_info(NAO_connection):
 
 # initialise the connection between the client and the server using the port number
 def TCP_connect(PORT_NUMBER):
-  return None
+  print ('Waiting for client connection...')
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+   
+  s.bind(("", PORT))
+   
+  s.listen()
+  conn, addr = s.accept()
+  return conn, s
+  
 
 # recieve the action info from the controller.py instance running on the lab PC and return this info in the:
 # [hip_angle,hip_angle_speed,knee_angle,knee_angle_speed] format
-def get_action(TCP_connection):
-  
+def get_action(TCP_connection, TCP_socket):
+  TCP_connection.sendall(pickle.dumps('Please send data'))    
+  data = pickle.loads(TCP_connection.recv(4096))
+  return data
   return [hip_angle,hip_angle_speed,knee_angle,knee_angle_speed]
 
 
 # send the NAO_info array in [hip_angle, knee_angle] format to the lab PC
-def send(NAO_info):
-  pass
+def send(NAO_info, s):
+  data = pickle.dumps(NAO_info, protocol=0, fix_imports=True)
+  s.sendall(data)
+  
 
   
 main()
